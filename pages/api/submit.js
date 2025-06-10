5ea0239cfd6ab490a25fcb084a7828ad651d672b
Dennis Skripalsh <dennisskripalsh8@gmail.com>
2025-06-10 11:02:46 -0700

    rollback

--- pages/api/submit.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method Not Allowed' });
-  }
-
-  const { name, email, company, icp, website } = req.body;
-  console.log('Lead submitted:', { name, email, company, icp, website });
-  return res.status(200).json({ success: true });
-}
